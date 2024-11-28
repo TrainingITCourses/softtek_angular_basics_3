60a5d9f19beb0e5ad8aaf88cf209b80527a458dd
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-28 13:26:46 +0100

    feat: validation helper

--- AstroBookings/src/app/routes/auth/register.page.ts
+++ AstroBookings/src/app/routes/auth/register.page.ts
@@ -1,20 +1,52 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   template: `
     <h2>🔏 Register</h2>
-    <form>
+    <form #f="ngForm">
       <fieldset>
         <section>
           <label for="username">User name</label>
-          <input id="username" type="text" placeholder="User name" />
+          <input
+            id="username"
+            name="username"
+            type="text"
+            placeholder="User name"
+            [(ngModel)]="username"
+            #usernameModel="ngModel"
+            required
+            minlength="3"
+            [attr.aria-invalid]="modelInvalid(usernameModel)"
+          />
+          @if(modelInvalid(usernameModel)){
+          <small>User name must be at least 3 characters long</small>
+          }
         </section>
         <section>
           <label for="email">Email</label>
-          <input id="email" name="email" type="email" placeholder="Email" />
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Email"
+            [(ngModel)]="email"
+            #emailModel="ngModel"
+            required
+            email
+            [attr.aria-invalid]="modelInvalid(emailModel)"
+          />
+          @if(modelInvalid(emailModel)){
+          <small>Invalid email</small>
+          }
         </section>
         <section>
           <label for="password">Password</label>
@@ -34,9 +66,23 @@ import { RouterLink } from '@angular/router';
           />
         </section>
       </fieldset>
-      <button type="submit">Register</button>
+      <button type="submit" [disabled]="f.invalid" (click)="register()">
+        Register
+      </button>
     </form>
     <a routerLink="../login"> 🔐 Login if already have an account</a>
   `,
 })
-export default class RegisterPage {}
+export default class RegisterPage {
+  protected username: WritableSignal<string> = signal('');
+  protected email: WritableSignal<string> = signal('');
+
+  protected modelInvalid(model: NgModel): boolean | undefined {
+    if (!model.touched) return undefined;
+    return model.invalid === true;
+  }
+
+  protected register(): void {
+    console.log('Register: ' + this.username());
+  }
+}
