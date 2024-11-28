f9100a165cf6d896bc67faa4940cefdb000a1fe9
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-28 12:09:19 +0100

    feat: forms html

--- AstroBookings/src/app/routes/auth/login.page.ts
+++ AstroBookings/src/app/routes/auth/login.page.ts
@@ -6,6 +6,19 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   template: `
     <h2>🔐 Login</h2>
+    <form>
+      <fieldset>
+        <section>
+          <label>Email</label>
+          <input type="email" placeholder="Email" />
+        </section>
+        <section>
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+        </section>
+      </fieldset>
+      <button type="submit">Login</button>
+    </form>
     <a routerLink="../register">🔏 Register if don´t have an account</a>
   `,
 })
--- AstroBookings/src/app/routes/auth/register.page.ts
+++ AstroBookings/src/app/routes/auth/register.page.ts
@@ -1,7 +1,33 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  template: ` <h2>🔏 Register</h2> `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink],
+  template: `
+    <h2>🔏 Register</h2>
+    <form>
+      <fieldset>
+        <section>
+          <label>User name</label>
+          <input type="text" placeholder="User name" />
+        </section>
+        <section>
+          <label>Email</label>
+          <input type="email" placeholder="Email" />
+        </section>
+        <section>
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+        </section>
+        <section>
+          <label>Confirm Password</label>
+          <input type="password" placeholder="Confirm Password" />
+        </section>
+      </fieldset>
+      <button type="submit">Register</button>
+    </form>
+    <a routerLink="../login"> 🔐 Login if already have an account</a>
+  `,
 })
 export default class RegisterPage {}
